5b9db3b4a2034f805017cfcdb86e336fc019b46e
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-03-30 16:40:03 +0530

    Add hrefs and html for morse and rsa

--- app.py
+++ app.py
@@ -9,5 +9,17 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/morse')
+def morse():
+
+    return render_template('morse.html')
+
+
+@app.route('/rsa')
+def rsa():
+
+    return render_template('rsa.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
